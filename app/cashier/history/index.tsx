97c23c6d8ae21d666792222af5fd67fdee0577f8
Synxularity <mosyam0202@gmail.com>
2026-03-04 13:13:31 +0700

    feat(history): add transactions history

---
+++ app/cashier/history/index.tsx
@@ -0,0 +1,7 @@
+// app/cashier/history/index.tsx
+import React from 'react';
+import TransactionHistoryPage from '../../../pages/TransactionHistoryPage';
+
+export default function CashierHistoryScreen() {
+  return <TransactionHistoryPage role="cashier" />;
+}
